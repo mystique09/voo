984816320dbbd55a7b057d7594252e92713e3423
mystique09 <knightkrypto7@gmail.com>
2025-04-21 18:52:40 +0800

    Add models crate with Gemini model implementation and dependencies

---
+++ crates/models/src/models/gemini.rs
@@ -0,0 +1,130 @@
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use domain::models::agent::{AgentClient, AgentError};
+use reqwest::Body;
+
+static API_URL: &'static str = "https://generativelanguage.googleapis.com/v1beta/models/";
+static MODEL: &'static str = "gemini-2.0-flash";
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct GeminiModel {
+    api_key: String,
+    reqwest: Arc<reqwest::Client>,
+}
+
+impl GeminiModel {
+    pub fn new(api_key: String) -> Self {
+        Self {
+            api_key,
+            reqwest: Arc::new(reqwest::Client::new()),
+        }
+    }
+}
+
+#[async_trait]
+impl AgentClient for GeminiModel {
+    async fn ask(&self, prompt: &str) -> Result<String, AgentError> {
+        let api_key = &self.api_key;
+        let url = format!("{}{}:generateContent?key={}", API_URL, MODEL, api_key);
+
+        let content = r#"{
+    "contents": [
+      {
+        "parts": [
+          {
+            "text": "{prompt}"
+          }
+        ]
+      }
+    ]
+  }"#
+        .replace("{prompt}", prompt);
+
+        let response = self
+            .reqwest
+            .post(url)
+            .body(Body::from(content))
+            .send()
+            .await
+            .map_err(|e| AgentError::AgentError(Some(e.to_string())))?;
+
+        let response_text = response
+            .text()
+            .await
+            .map_err(|e| AgentError::AgentError(Some(e.to_string())))?;
+        let response_json: GeminiResponse = serde_json::from_str(&response_text)
+            .map_err(|e| AgentError::AgentError(Some(e.to_string())))?;
+        let text = response_json
+            .candidates
+            .get(0)
+            .unwrap()
+            .content
+            .parts
+            .get(0)
+            .unwrap()
+            .text
+            .clone();
+
+        Ok(text)
+    }
+
+    async fn perform_tool(&self, prompt: &str) -> Result<String, AgentError> {
+        Ok(prompt.to_string())
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GeminiResponse {
+    pub candidates: Vec<Candidate>,
+    pub usage_metadata: UsageMetadata,
+    pub model_version: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Candidate {
+    pub content: Content,
+    pub finish_reason: String,
+    pub avg_logprobs: f64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Content {
+    pub parts: Vec<Part>,
+    pub role: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Part {
+    pub text: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UsageMetadata {
+    pub prompt_token_count: i64,
+    pub candidates_token_count: i64,
+    pub total_token_count: i64,
+    pub prompt_tokens_details: Vec<PromptTokensDetail>,
+    pub candidates_tokens_details: Vec<CandidatesTokensDetail>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PromptTokensDetail {
+    pub modality: String,
+    pub token_count: i64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CandidatesTokensDetail {
+    pub modality: String,
+    pub token_count: i64,
+}
